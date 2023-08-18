76b822869118873db52c141c9f3a8bf112b504f8
Juan Diego Moncaleano Quiñones <juan.moncaleano@interedes.com.co>
2023-08-18 09:49:48 -0500

    Creacion de los test de cargos y empresas asociadas

--- src/test/java/PositionsCrudTest.java
+++ src/test/java/PositionsCrudTest.java
@@ -1,20 +1,26 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import io.restassured.response.*;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+import java.util.Objects;
 
 import static io.restassured.RestAssured.given;
 
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PositionsCrudTest {
 
     private String token;
     private String appQa = "application";
-    private Integer Positions;
+    private String IdPosition;
+    private String IdDelete;
+    private String tenant= "INTEREDES";
     private String applicationQa= "siie.qa.interedes.com.co";
     @BeforeEach
-    public void positionsCrud(){
+    public void getTokenTest(){
 
         RestAssured.baseURI= "https://siie.qa.interedes.com.co/services";
 
@@ -35,11 +41,13 @@ public class PositionsCrudTest {
         //System.out.println(token);
     }
 
-@Test
-    public void getAllPositions(){
+    @Order(1)
+    @Test()
+    public void getAllPositionsTest(){
         given()
                 .header(appQa, applicationQa)
                 .header("Authorization", token)
+                .header("tenant",tenant)
                 .contentType(ContentType.JSON)
                 .body("{\n" +
                         "    \"filters\": [\n" +
@@ -58,11 +66,191 @@ public class PositionsCrudTest {
                         "}")
                 .post("/dynamic-service/services/user-service/job/v1/get_jobs_criteria")
                 .then()
+                .log()
+                .all()
                 .statusCode(200)
                 .extract()
                 .body()
                 .asString();
             }
 
-            @Test
+    @Order(2)
+    @Test
+    public void createPositionTest(){
+
+        IdPosition =    given()
+                 .log()
+                 .all()
+                .header(appQa,applicationQa)
+                .header("Authorization", token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"title\": \"SCRUM MASTER \",\n" +
+                        "    \"status\": 1\n" +
+                        "}")
+                .post("/user-service/job/v1/create_job")
+                .then()
+                 .log()
+                 .all()
+                .statusCode(200)
+                .extract()
+                .path("data.id")
+                .toString();
+
+        System.out.println(IdPosition);
+
+    }
+
+    @Order(3)
+    @Test
+    public void updatePositionTest(){
+
+                 given()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "     \"id\":"+ IdPosition + " ,\n" +
+                        "        \"title\": \"SCRUM MASTER SENIOR\",\n" +
+                        "        \"status\": 1\n" +
+                        "}")
+                .post("/user-service/job/v1/update_job")
+                .then()
+                .extract()
+                .toString();
+
+    }
+
+
+
+    @Order(4)
+    @Test
+    public void positionAssignedToACompany(){
+        given()
+                .header(appQa, applicationQa)
+                .header("Authorization", token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"companyId\": 2,\n" +
+                        "    \"jobId\":"+IdPosition+",\n" +
+                        "    \"status\": 1\n" +
+                        "}")
+                .post("/dynamic-service/services/user-service/company/v1/company_job")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .path("data.id");
+    }
+
+
+    @Order(5)
+    @Test
+    public void deletePositionAssignedToACompany(){
+        given()
+                .header(appQa, applicationQa)
+                .header("Authorization", token)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"companyId\": 2,\n" +
+                        "    \"jobId\":"+IdPosition+",\n" +
+                        "    \"status\": 1\n" +
+                        "}")
+                .put("/dynamic-service/services/user-service/company/v1/remove_company_job")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .toString();
+    }
+
+
+    @Order(6)
+    @Test
+    public void deletePositionTest(){
+        IdPosition =
+                given()
+                .header(appQa, applicationQa)
+                .header("Authorization", token)
+                .header("tenant",tenant)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"filters\": [\n" +
+                        "     \n" +
+                        "    ],\n" +
+                        "    \"sorts\": [\n" +
+                        "        {\n" +
+                        "         \"key\":\"id\" ,\n" +
+                        "         \"direction\": \"ASC\"\n" +
+                        "      }\n" +
+                        "\n" +
+                        "\n" +
+                        "    ],\n" +
+                        "    \"page\": null,\n" +
+                        "    \"size\": null\n" +
+                        "}")
+                .post("/dynamic-service/services/user-service/job/v1/get_jobs_criteria")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .body()
+                .asString();
+
+        JsonPath arregloPositions = new JsonPath(IdPosition);
+        List<Objects> PositionsArray = arregloPositions.getList("data");
+        int Cant = PositionsArray.size()-1;
+
+
+
+        IdDelete =
+                given()
+                .header(appQa, applicationQa)
+                .header("Authorization", token)
+                .header("tenant",tenant)
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"filters\": [\n" +
+                        "     \n" +
+                        "    ],\n" +
+                        "    \"sorts\": [\n" +
+                        "        {\n" +
+                        "         \"key\":\"id\" ,\n" +
+                        "         \"direction\": \"ASC\"\n" +
+                        "      }\n" +
+                        "\n" +
+                        "\n" +
+                        "    ],\n" +
+                        "    \"page\": null,\n" +
+                        "    \"size\": null\n" +
+                        "}")
+                .post("/dynamic-service/services/user-service/job/v1/get_jobs_criteria")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .path("data.id["+Cant+"]")
+                .toString();
+
+
+
+        given()
+                .header(appQa,applicationQa)
+                .contentType(ContentType.JSON)
+                .header("Authorization", token )
+                .log()
+                .all()
+                .delete("/user-service/job/v1/delete_job/"+IdDelete)
+                .then()
+                .log()
+                .all()
+                .toString();
+
+
+    }
 }
