b277a8e725ca4da5cf009e1058c15eb55066f2ae
Juan Diego Moncaleano Quiñones <juan.moncaleano@interedes.com.co>
2023-08-29 13:45:15 -0500

    Se finalizan las pruebas del submodulo de empresas asocciadas

--- src/test/java/ContractorCompaniesCrudTest.java
+++ src/test/java/ContractorCompaniesCrudTest.java
@@ -17,7 +17,9 @@ public class ContractorCompaniesCrudTest {
     private String applicationQa= "siie.qa.interedes.com.co";
     private  String cantCompanies;
     private  String idCreatedCompany;
-
+    private String updatedContractorCompany;
+    private String lastPositionCompany;
+    private String companyName;
 
     @Order(1)
     @BeforeEach
@@ -91,6 +93,181 @@ public class ContractorCompaniesCrudTest {
     @Test
     public void createContractorCompanyTest(){
 
+        idCreatedCompany = given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"name\":\"Dillancol\",\n" +
+                        "    \"identificationTypeId\": 1,\n" +
+                        "    \"idNumber\": 7654243,\n" +
+                        "    \"checkDigit\": 6,\n" +
+                        "    \"address\": \"Cra. 7 #2-63, Neiva, Huila\",\n" +
+                        "    \"cityId\": 1,\n" +
+                        "    \"phone\": \"88707448\",\n" +
+                        "    \"email\": \"dillancolcomercial@gmail.com\",\n" +
+                        "    \"personTypeId\": 1,\n" +
+                        "    \"status\": 1,\n" +
+                        "    \"main\": 1\n" +
+                        "}")
+                .post("/dynamic-service/services/parameters-service/company/v1/create")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .path("data.id")
+                .toString();
+
+        System.out.println(idCreatedCompany);
+
+    }
+
+    @Order(3)
+    @Test
+    public void updateContractorCompany(){
+        updatedContractorCompany =  given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"id\":"+idCreatedCompany+", \n" +
+                        "    \"name\":\"Dillancol S.A.S\",\n" +
+                        "    \"identificationTypeId\":1, \n" +
+                        "    \"indentificationTypeDTO\":null, \n" +
+                        "    \"idNumber\":7654253, \n" +
+                        "    \"checkDigit\":6, \n" +
+                        "    \"address\":\"Cra. 7 #2-68\", \n" +
+                        "    \"latitude\":0, \n" +
+                        "    \"longitude\":0, \n" +
+                        "    \"branchTypeId\":1, \n" +
+                        "    \"cityId\":1, \n" +
+                        "    \"cityDTO\":null, \n" +
+                        "    \"phone\":88707448, \n" +
+                        "    \"email\":\"dillancolcomercial@gmail.com\", \n" +
+                        "    \"contactName\":null, \n" +
+                        "    \"personTypeId\":1, \n" +
+                        "    \"personTypeDTO\":null, \n" +
+                        "    \"status\":0, \n" +
+                        "    \"main\":1, \n" +
+                        "    \"companyId\":null, \n" +
+                        "    \"companyName\":null\n" +
+                        "    }")
+                .put("/dynamic-service/services/parameters-service/company/v1/update")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .body()
+                .asString();
+
+        System.out.println(updatedContractorCompany);
+    }
+
+    @Order(4)
+    @Test
+    public void validateContractorCompanieName(){
+        lastPositionCompany =
+        given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"filters\": [\n" +
+                        "        {\n" +
+                        "            \"key\": \"name\",\n" +
+                        "            \"operator\": \"LIKE\",\n" +
+                        "            \"field_type\": \"STRING\",\n" +
+                        "            \"value\": \"\"\n" +
+                        "        }\n" +
+                        "    ],\n" +
+                        "    \"sorts\": [\n" +
+                        "        {\n" +
+                        "            \"key\":\"id\",\n" +
+                        "            \"direction\": \"ASC\"\n" +
+                        "        }\n" +
+                        "    ],\n" +
+                        "    \"page\": null,\n" +
+                        "    \"size\": null\n" +
+                        "}")
+                .post("/dynamic-service/services/parameters-service/company/v1/get_companies_criteria")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .body()
+                .asString();
+
+        JsonPath arregloPositions = new JsonPath(lastPositionCompany);
+        List<Objects> PositionsArray = arregloPositions.getList("data");
+        int Cant = PositionsArray.size()-1;
+
+
+
+        companyName =
+        given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"filters\": [\n" +
+                        "        {\n" +
+                        "            \"key\": \"name\",\n" +
+                        "            \"operator\": \"LIKE\",\n" +
+                        "            \"field_type\": \"STRING\",\n" +
+                        "            \"value\": \"\"\n" +
+                        "        }\n" +
+                        "    ],\n" +
+                        "    \"sorts\": [\n" +
+                        "        {\n" +
+                        "            \"key\":\"id\",\n" +
+                        "            \"direction\": \"ASC\"\n" +
+                        "        }\n" +
+                        "    ],\n" +
+                        "    \"page\": null,\n" +
+                        "    \"size\": null\n" +
+                        "}")
+                .post("/dynamic-service/services/parameters-service/company/v1/get_companies_criteria")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .path("data["+Cant+"].name");
+
+        Assertions.assertEquals("Dillancol S.A.S",companyName);
+        System.out.println("La empresa asociada se le actualizo correctamente el nombre por: "+companyName);
+
+    }
+
+
+    @Order(5)
+    @Test
+    public void delteContractorCompany(){
+
+
+        given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .delete("/dynamic-service/services/user-service/company/v1/delete_company/"+idCreatedCompany)
+                .then()
+                .statusCode(200);
+
+        System.out.println("Se elimino la empresa con ID: "+ idCreatedCompany +" correctamente.");
 
     }
 
