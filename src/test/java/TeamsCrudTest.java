f6d326e33cfc454635b4394f421289ad2a8183f3
Juan Diego Moncaleano Quiñones <juan.moncaleano@interedes.com.co>
2023-10-04 11:48:49 -0500

    se continua creando las pruebas de equipo, las cuales e agregan llamar todos los equipos, listar roles y listar usuarios

--- src/test/java/TeamsCrudTest.java
+++ src/test/java/TeamsCrudTest.java
@@ -42,10 +42,44 @@ public class TeamsCrudTest {
 
     }
 
-
     @Order(2)
     @Test
-    public void CreateTeam(){
+    public void listRoles(){
+
+        given()
+                .log()
+                .all()
+                .header(appQa,applicationQa)
+                .header("Authorization",token)
+                .header("tenant","INTEREDES")
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"filters\": [],\n" +
+                        "    \"sorts\": [{\n" +
+                        "            \"key\":\"id\",\n" +
+                        "            \"direction\":\"ASC\"\n" +
+                        "        }],\n" +
+                        "    \"page\": null,\n" +
+                        "    \"size\": null\n" +
+                        "}")
+                .post("/dynamic-service/services/user-service/rol/v1/get_roles_criteria")
+                .then()
+                .log()
+                .all()
+                .extract()
+                .body();
+    }
+
+    @Order(3)
+    @Test
+    public void listUsers(){
+
+    }
+
+
+    @Order(4)
+    @Test
+    public void search(){
 
     }
 
